f11879704b9ca79aba1e75123a32c0f633662ecc
Carter David Henning <carterdavidhenning@gmail.com>
2022-02-25 17:21:57 -0500

    Add showing test sprite

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Bevy Jam #1".to_string(),
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .run();
@@ -9,4 +14,8 @@ fn main() {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("Kenney Blue Letter Tiles/letter_A.png"),
+        ..Default::default()
+    });
 }
